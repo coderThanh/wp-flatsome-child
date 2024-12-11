b7db67e071f0226078d6515f63b20ff87109db0e
Thanh Nextjs/Flutter <nguyenvanthanh6499@gmail.com>
2024-12-11 14:15:34 +0700

    update marquee

--- shortcode/marquee-multi-item/index.php
+++ shortcode/marquee-multi-item/index.php
@@ -6,72 +6,108 @@
  */
 
 
-add_action('wp_enqueue_scripts', 'pt_shortcode_marquee_multi_item_setup');
+add_action( 'wp_enqueue_scripts', 'pt_shortcode_marquee_multi_item_setup' );
 
 function pt_shortcode_marquee_multi_item_setup()
 {
-    wp_enqueue_style('marquee-multi-item-shortcode', get_stylesheet_directory_uri() . '/shortcode/marquee-multi-item/syles.css', [], '1.0');
+	wp_enqueue_style( 'marquee-multi-item-shortcode', get_stylesheet_directory_uri() . '/shortcode/marquee-multi-item/syles.css', [], '1.0' );
 }
 
 //
-add_action('ux_builder_setup', 'pt_ux_builder_marquee_milti_item');
+add_action( 'ux_builder_setup', 'pt_ux_builder_marquee_milti_item' );
 
 function pt_ux_builder_marquee_milti_item()
 {
-    add_ux_builder_shortcode('pt-marquee-multi-item', array(
-        'name'      => __('Pt Marquee Multi Item'),
-        'category'  => __('Content'),
-        'priority'  => 10,
-        'type' => 'container',
-        'options' => [
-            'advanced_options' => require(THEME_CHILD_ROOT . '/../flatsome/inc/builder/shortcodes/commons/advanced.php'),
-        ],
-    ));
+	add_ux_builder_shortcode( 'pt-marquee-multi-item', array(
+		'name'     => __( 'Pt Marquee Multi Item' ),
+		'category' => __( 'Content' ),
+		'priority' => 10,
+		'type'     => 'container',
+		'options'  => [ 
+			'is_pause'         => array(
+				'type'    => 'checkbox',
+				'heading' => 'Pause',
+				'default' => 'false',
+			),
+			'duration'         => array(
+				'type'        => 'textfield',
+				'heading'     => 'seconds',
+				'responsive'  => true,
+				'placeholder' => '5s',
+			),
+			'advanced_options' => require( THEME_CHILD_ROOT . '/../flatsome/inc/builder/shortcodes/commons/advanced.php' ),
+		],
+	) );
 }
 
 
 //
-add_shortcode('pt-marquee-multi-item', 'pt_get_marquee_milti_item');
+add_shortcode( 'pt-marquee-multi-item', 'pt_get_marquee_milti_item' );
 
 function pt_get_marquee_milti_item($atts, $content = null)
 {
-    extract(shortcode_atts(array(
-        'class' => '',
-        'visibility' => '',
-    ), $atts));
+	extract( shortcode_atts( array(
+		'class'        => '',
+		'visibility'   => '',
+		'is_pause'     => 'false',
+		'duration'     => '5s',
+		'duration__md' => '',
+		'duration__sm' => '',
+	), $atts ) );
 
-    ob_start();
+	$style      = '';
+	$style_item = '';
 
-?>
-    <div class="marquee-wrap <?php echo esc_attr($class); ?> <?php echo esc_attr($visibility); ?>">
-        <div class="marquee-inner marquee-animation">
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-            <div class="item">
-                <?php echo do_shortcode($content); ?>
-            </div>
-        </div>
-    </div>
-<?php
+	if( !empty( $duration ) ) {
+		$style .= '--duration: ' . $duration . ';';
+	}
 
-    return ob_get_clean();
+	if( !empty( $duration__md ) ) {
+		$style .= '--duration-md: ' . $duration__md . ';';
+	}
+
+	if( !empty( $duration__sm ) ) {
+		$style .= '--duration-sm: ' . $duration__sm . ';';
+	}
+
+
+	if( $is_pause == 'true' ) {
+		$style_item .= 'animation-play-state: paused;';
+	}
+
+	ob_start();
+
+	?>
+	<div class="marquee-wrap <?php echo esc_attr( $class ); ?> <?php echo esc_attr( $visibility ); ?>"
+		style="<?php echo esc_attr( $style ); ?>">
+		<div class="marquee-inner marquee-animation">
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+			<div class="item" style="<?php echo esc_attr( $style_item ); ?>">
+				<?php echo do_shortcode( $content ); ?>
+			</div>
+		</div>
+	</div>
+	<?php
+
+	return ob_get_clean();
 }
