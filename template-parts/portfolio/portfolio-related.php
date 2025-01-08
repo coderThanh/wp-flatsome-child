740ca79c1e2f83f1e55e574cdcaf452117627cbf
Thanh Nextjs/Flutter <nguyenvanthanh6499@gmail.com>
2025-01-08 14:11:59 +0700

    wrap container for portfolio related single

--- template-parts/portfolio/portfolio-related.php
+++ template-parts/portfolio/portfolio-related.php
@@ -15,4 +15,4 @@ $term_id = $terms ? current( $terms )->term_id : '';
 $height  = get_theme_mod( 'portfolio_height' );
 $height  = $height ? $height : '';
 
-echo do_shortcode( '<div class="portfolio-related"><h3>OTHER PORTFOLIO</h3>[ux_portfolio image_height="' . $height . '" class="portfolio-related" exclude="' . get_the_ID() . '" cat="' . $term_id . '"   columns="3" columns__md="2" columns__sm="2"]</div>' );
+echo do_shortcode( '<div class="portfolio-related"><div class="container"><h3>OTHER PORTFOLIO</h3>[ux_portfolio image_height="' . $height . '" class="portfolio-related" exclude="' . get_the_ID() . '" cat="' . $term_id . '"   columns="2" columns__md="1" columns__sm="1"]</div></div>' );
